4790f2aaa2ec6d3d6c53e047e88d2eaadd2b3dcb
navyakaturi <102217159+navyakaturi@users.noreply.github.com>
2022-10-26 23:41:31 +0530

    n=int(input()) a=list(map(int,input().split())) x=[] for i in a:     rem=0     y=i     while i!=0:         s=i//10         r=i%10         rem=rem*10+r         i=s     if y==rem:         x.append(rem) print(len(x))

---
+++ Count_palindromes.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=list(map(int,input().split()))
+x=[]
+for i in a:
+    rem=0
+    y=i
+    while i!=0:
+        s=i//10
+        r=i%10
+        rem=rem*10+r
+        i=s
+    if y==rem:
+        x.append(rem)
+print(len(x))
