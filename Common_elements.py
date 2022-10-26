512d7941e5640d2d1790a619551d81254e6d6bc3
navyakaturi <102217159+navyakaturi@users.noreply.github.com>
2022-10-26 22:39:31 +0530

    n,m=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) x=[] for i in range(0,n):     if a[i] in x:         continue     if a[i] in b:         x.append(a[i]) print(*x)

---
+++ Common_elements.py
@@ -0,0 +1,10 @@
+n,m=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+x=[]
+for i in range(0,n):
+    if a[i] in x:
+        continue
+    if a[i] in b:
+        x.append(a[i])
+print(*x)
