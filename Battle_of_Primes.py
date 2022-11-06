f0837990f335556c54a0ec6e6d948bcc1432ec8b
navyakaturi <102217159+navyakaturi@users.noreply.github.com>
2022-11-06 14:32:09 +0530

    n1=int(input()) n2=int(input()) def isprim(n):     if n==1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     return True c=n1+n2+1 while(True):     if isprim(c):         break     c+=1 print(c-n1-n2)

---
+++ Battle_of_Primes.py
@@ -0,0 +1,16 @@
+n1=int(input())
+n2=int(input())
+def isprim(n):
+    if n==1:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+c=n1+n2+1
+while(True):
+    if isprim(c):
+        break
+    c+=1
+print(c-n1-n2)
+        
