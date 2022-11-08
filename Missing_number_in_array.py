10db9868dc38d55e6b75eb4356a95f80b1d05744
navyakaturi <102217159+navyakaturi@users.noreply.github.com>
2022-11-08 23:14:49 +0530

    t=int(input()) for k in range(t):     n=int(input())     l=list(map(int,input().split()))     for i in range(1,n+1):         if i not in l:             print(i)             break

---
+++ Missing_number_in_array.py
@@ -0,0 +1,8 @@
+t=int(input())
+for k in range(t):
+    n=int(input())
+    l=list(map(int,input().split()))
+    for i in range(1,n+1):
+        if i not in l:
+            print(i)
+            break
